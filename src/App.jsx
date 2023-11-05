5cddb54535e15ea83822621fa8510eecdfa07e4a
Uchenna <ansonuchenna@gmail.com>
2023-11-05 06:15:29 +0000

    Building a color generator

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Form from './Form'
+import ColorList from "./ColorList";
+import Values from 'values.js'
+import { useState } from 'react'
+
+
+const App = () => {
+  const[colors, setColors] = useState(new Values('#f15025').all(10))
+
+  return (
+  <main>
+    <Form/>
+    <ColorList colors={colors}/>
+    </main>
+  );
+};
+export default App;
---
+++ src/SingleColor.jsx
@@ -0,0 +1,10 @@
+const SingleColor = ({index, color}) => {
+    const {hex, weight} = color;
+  return (
+    <article className={index > 10 ? ' color color-light' : 'color'} style={{background:`#${hex}`}}>
+        <p className="color-value">{weight}%</p>
+        <p className='color-value'>#{hex}</p>
+    </article>
+  )
+}
+export default SingleColor
