afb1c001aa802f7ca6f1dd5b8ae470d86a5634e8
Uchenna <ansonuchenna@gmail.com>
2023-11-05 07:52:11 +0000

    Updates on the Color Generator

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,7 +1,23 @@
+import {toast} from 'react-toastify'
+
 const SingleColor = ({index, color}) => {
     const {hex, weight} = color;
+    const saveToClipboard = async () => {
+      if(navigator.clipboard) {
+        try {
+          await navigator.clipboard.writeText(`#${hex}`)
+          toast.success('Color copied to clipboard')
+        } catch (error){
+          toast.error('Failed to copy to clipboard')
+        }
+      } else {
+        toast.error('Clipboard access not available')
+      }
+    }
+
+
   return (
-    <article className={index > 10 ? ' color color-light' : 'color'} style={{background:`#${hex}`}}>
+    <article className={index > 10 ? ' color color-light' : 'color'} style={{background:`#${hex}`}} onClick={saveToClipboard}>
         <p className="color-value">{weight}%</p>
         <p className='color-value'>#{hex}</p>
     </article>
